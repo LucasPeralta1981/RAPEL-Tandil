e1ba4ee666d55c1f14f879fc19f02ade5f41c4e1
LucasPeralta1981 <146029137+LucasPeralta1981@users.noreply.github.com>
2026-05-28 13:18:53 -0300

    2

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -1,6 +1,6 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
 import connectDB from './db';
-import User from '../models/User';
+import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 
 export const authOptions = {
